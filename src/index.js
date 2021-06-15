4bdca95401fb43c0d3e14a666ca10f1225501d17
Alejandro <Velasquez>
2021-06-14 22:07:36 -0500

    feat: soporte para JSON

--- src/index.js
+++ src/index.js
@@ -3,9 +3,25 @@ const http = require('http');
 const server = http.createServer((req, res)=> {
     const { url, method } = req;
     console.log(`URL: ${url} - Method: ${method}`);
-    res.writeHead(200, {'Content-Type':'text/plain'});
-    res.write('Received');
-    res.end();
+
+    switch(method) {
+        case "GET":
+            if (url === "/") {
+                res.writeHead(200, {'Content-Type':'application/json'});
+                res.write(JSON.stringify({message: 'Server is working'}));
+                res.end();
+            
+            } else {
+                res.writeHead(200, {'Content-Type':'application/json'});
+                res.write(JSON.stringify({message: '404 NOT FOUND'}));
+                res.end();
+            }
+       // case "POST":
+        //case  "PUT":
+        //case "DELETE":
+        //default:
+
+    }
 })
 
 server.listen(5000);    
